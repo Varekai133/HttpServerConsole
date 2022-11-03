d5113ec5afc2ac09653ca414608a5aefbf4fb01c
Varekai133 <stebleva.cs@gmail.com>
2022-11-03 21:33:15 +0300

    add server to separate assembly

---
+++ HttpServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using Server.Itself;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ServerHost serverHost = new ServerHost();
+        serverHost.Start();
+    }
+}
---
+++ Server.Itself/ServerHost.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server.Itself;
+public class ServerHost {
+    public ServerHost() {
+        
+    }
+
+    public void Start() {
+        TcpListener listener = new(IPAddress.Any, 80);
+        listener.Start();
+
+        while (true) {
+            var client = listener.AcceptTcpClient();
+            using (var stream = client.GetStream()) {
+                using (var reader = new StreamReader(stream))
+                using (var writer = new StreamWriter(stream)) {
+                    for (string line = null; line != string.Empty; line = reader.ReadLine()) {
+                        Console.WriteLine(line);
+                    }
+
+                    writer.Write("Hello from server!");
+                }
+            }
+        }
+    }
+}
